b7a84fab49aa57589968f26fd15643c0e08a431b
Hugo Pascual <hugo.pascual.adan@gmail.com>
2023-04-08 18:37:17 +0200

    WIP root_servers campaigns

--- campaign.sh
+++ campaign.sh
@@ -5,84 +5,52 @@ alpha_array=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
 #areas_array=("North-Central" "North-East" "South-Central" "South-East" "West" "WW")
 #areas_array=("North-Central" "WW")
 areas_array=("North-Central")
-root_server_ip_direcction_selected="192.5.5.241"
-campaign_log_file="Campaign_"$root_server_ip_direcction_selected"_$(date +%F_%T).txt"
-alpha_tunning_log_file="Alpha_tunning_"$root_server_ip_direcction_selected"_$(date +%F_%T).txt"
-measurements_filenames_array=()
+
+root_servers_ip_directions=(
+"198.41.0.4" "199.9.14.201" "192.33.4.12" "199.7.91.13" "192.203.230.10"
+"192.112.36.4" "198.97.190.53" "192.36.148.17" "192.58.128.30"
+"193.0.14.129" "199.7.83.42" "202.12.27.33")
+root_server_ip_direction_selected="192.5.5.241"
+root_servers_names=(
+"A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M"
+)
+
 probes_filenames_array=()
 probes_filepaths_array=()
 
-fill_measurements_filenames_filenames_array()
-{
-  for probes_filename in "${probes_filenames_array[@]}"; do
-    measurements_filenames_array+=($probes_filename"_"$root_server_ip_direcction_selected.json)
-  done
-}
-
-fill_probes_filenames_array()
+fill_probes_arrays()
 {
   for area in "${areas_array[@]}"; do
     for num_probes in "${num_probes_array[@]}"; do
       probes_filenames_array+=($area"_"$num_probes)
-      probes_filepaths_array+=("probes_sets/"$area"_"$num_probes.json)
+      probes_filepaths_array+=("datasets/probes_sets/"$area"_"$num_probes.json)
     done
   done
 }
 
-measurement_manual()
+measurement_campaign_ip_selected()
 {
-  ./igreedy.sh -m "192.203.230.10" \
-  -p "probes_sets/North-Central_1000.json" \
-  -o "North-Central_1000_192.203.230.10" \
-  | tee -a "Single_North-Central_1000_$(date +%F_%T).txt"
-
-  ./igreedy.sh -i "datasets/measurement/192.5.5.241-50948678-1678879591" \
-  -o "North-Central_100_1_192.5.5.241" \
-  -a "2"
-}
-
-measurement_big()
-{
+  campaign_log_file="Campaign_"$1"_$(date +%F_%T).txt"
   for probes_filepath in "${probes_filepaths_array[@]}"; do
-      echo """
-Measurement to $root_server_ip_direcction_selected using $probes_filepath 
-started."""
+    echo ./igreedy.sh -m "$1" \
+    -p "$probes_filepath" \
+    | tee -a "campaigns_logs/$campaign_log_file"
 
-      echo ./igreedy.sh -m $root_server_ip_direcction_selected \
-      -p "$probes_filepath" \
-      | tee -a "$campaign_log_file"
+    #Start the measurements and save the results in a txt file
+    ./igreedy.sh -m "$1" \
+    -p "$probes_filepath" \
+    | tee -a "campaigns_logs/$campaign_log_file"
 
-      #Start the measurements and save the results in a txt file
-      ./igreedy.sh -m $root_server_ip_direcction_selected \
-      -p "$probes_filepath" \
-      | tee -a "$campaign_log_file"
-
-      echo "########################################" >> "$campaign_log_file"
-
-      echo """
-Measurement to $root_server_ip_direcction_selected using $probes_filepath 
-finished."""
+    echo "########################################" >> "campaigns_logs/$campaign_log_file"
   done
 }
 
 analyze_alpha()
 {
-  for measurement_filename in "${measurements_filenames_array[@]}"; do
-    for alpha in "${alpha_array[@]}"; do
-      echo ./igreedy.sh -i "datasets/measurement/$measurement_filename" \
-      -a "$alpha" \
-      | tee -a "$alpha_tunning_log_file"
-
-      ./igreedy.sh -i "datasets/measurement/$measurement_filename" \
-      -a "$alpha" \
-      | tee -a "$alpha_tunning_log_file"
-    done 
-    echo "########################################" >> "$alpha_tunning_log_file"
-  done
+  alpha_tunning_log_file="Alpha_tunning_"$1"_$(date +%F_%T).txt"
 }
 
-fill_probes_filenames_array
-fill_measurements_filenames_filenames_array
-
-measurement_big
-analyze_alpha
+fill_probes_arrays
+for ip in "${root_servers_ip_directions[@]}"; do
+  measurement_campaign_ip_selected "$ip"
+done
