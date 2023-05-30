9f1592ac13a1b7038c249dea674687924c1c14c9
hugopascual <hugopascual998@gmail.com>
2023-05-30 15:55:23 +0200

    WIP

--- code/validate_hunter_anycast_cloudfare.py
+++ code/validate_hunter_anycast_cloudfare.py
@@ -26,7 +26,7 @@ class AnycastValidationCloudfare:
             for target in self._targets_list:
                 output_filename = \
                     "datasets/hunter_measurements/campaigns/" \
-                    "validation_anycast_udp_cloudfare_{}_1_no_check_multi_ip_last_hop/{}_{}.json".format(
+                    "validation_anycast_udp_cloudfare_{}_3_no_check_multi_ip_last_hop/{}_{}.json".format(
                         today,
                         target,
                         vpn_server)
