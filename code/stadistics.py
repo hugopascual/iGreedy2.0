e26a268c7270d5bfaf2936cdcd964386a2aae2cf
hugopascual <hugopascual998@gmail.com>
2023-06-01 17:16:10 +0200

    HUNTER validations

--- code/stadistics.py
+++ code/stadistics.py
@@ -7,7 +7,8 @@ from shapely import Point
 from shapely import from_geojson
 # internal modules imports
 from utils.constants import (
-    HUNTER_MEASUREMENTS_CAMPAIGNS_PATH
+    HUNTER_MEASUREMENTS_CAMPAIGNS_PATH,
+    HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH
 )
 from utils.common_functions import (
     get_list_files_in_path,
@@ -19,16 +20,11 @@ from utils.common_functions import (
 
 class Statistics:
 
-    def __init__(self, tool: str, validation_campaign_directory: str):
+    def __init__(self,
+                 validation_campaign_directory: str,
+                 output_filename: str):
         self._validation_campaign_directory = validation_campaign_directory
-
-        if tool.lower() == "hunter":
-            self.hunter_build_statistics_validation_campaign()
-        elif tool.lower() == "igreedy":
-            self.igreedy_build_statistics_validation_campaign()
-        else:
-            print("No proper tool solicited")
-            return
+        self._output_filename = output_filename
 
     def igreedy_build_statistics_validation_campaign(self):
         return
@@ -79,24 +75,25 @@ class Statistics:
             )
 
         validation_results_df.to_csv(
-            HUNTER_MEASUREMENTS_CAMPAIGNS_PATH + "statistics_" +
-            self._validation_campaign_directory + ".csv",
+            HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH + "statistics_" +
+            self._output_filename + ".csv",
             sep=",")
 
         dict_to_json_file(
             results_not_valid,
-            HUNTER_MEASUREMENTS_CAMPAIGNS_PATH + "results_not_valid_" +
-            self._validation_campaign_directory + ".json")
+            HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH +
+            "results_not_valid_" + self._output_filename + ".json")
 
     # Auxiliary functions
     def calculate_hunter_result_outcome_country(self, results: dict) -> \
             (str, str):
-
-        try:
-            if len(results["hops_directions_list"][-1]) != 1:
-                return "Indeterminate", "Multiples IP on target hop"
-        except:
-            print("Target directions list exception")
+        #try:
+        #    if len(results["hops_directions_list"][-1]) != 1:
+        #        return "Indeterminate", "Multiples IP on target hop"
+        #    elif results["hops_directions_list"][-1] == "*":
+        #        return "Indeterminate", "Target hop do not respond"
+        #except:
+        #    print("Target directions list exception")
 
         num_result_countries = len(results["hunt_results"]["countries"])
         if num_result_countries == 1:
@@ -124,11 +121,13 @@ class Statistics:
 
     def calculate_hunter_result_outcome_city(self, results: dict) -> \
             (str, str):
-        try:
-            if len(results["hops_directions_list"][-1]) != 1:
-                return "Indeterminate", "Multiples IP on target hop"
-        except:
-            print("Target directions list exception")
+        #try:
+        #    if len(results["hops_directions_list"][-1]) != 1:
+        #        return "Indeterminate", "Multiples IP on target hop"
+        #    elif results["hops_directions_list"][-1] == "*":
+        #        return "Indeterminate", "Target hop do not respond"
+        #except:
+        #    print("Target directions list exception")
 
         num_result_cities = len(results["hunt_results"]["cities"])
         if num_result_cities == 1:
@@ -154,8 +153,67 @@ class Statistics:
         else:
             return "Indeterminate", "Too many cities"
 
+    def aggregate_hunter_statistics(self):
+        statistics_files = [filename for filename in
+                            get_list_files_in_path(
+                                HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH)
+                            if "statistics" in filename]
+        aggregation_df = pd.DataFrame(columns=[
+            "filename",
+            "country_positive_percent", "country_negative_percent",
+            "country_indeterminate_percent",
+            "country_positives", "country_negatives", "country_indeterminates",
+            "city_positive_percent", "city_negative_percent",
+            "city_indeterminate_percent",
+            "city_positives", "city_negatives", "city_indeterminates"
+        ])
+        for statistic_file in statistics_files:
+            statistic_df = pd.read_csv(
+                HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH + statistic_file)
+            total = len(statistic_df.index)
 
-Statistics("hunter", "validation_anycast_udp_cloudfare_20230530_3_no_check_multi_ip_last_hop")
+            country_positives = len(
+                statistic_df[statistic_df['country_outcome'] == "Positive"])
+            country_negatives = len(
+                statistic_df[statistic_df['country_outcome'] == "Negative"])
+            country_indeterminates = len(
+                statistic_df[statistic_df['country_outcome'] ==
+                             "Indeterminate"])
 
+            city_positives = len(
+                statistic_df[statistic_df['city_outcome'] == "Positive"])
+            city_negatives = len(
+                statistic_df[statistic_df['city_outcome'] == "Negative"])
+            city_indeterminates = len(
+                statistic_df[statistic_df['city_outcome'] == "Indeterminate"])
+
+            aggregation_df = pd.concat(
+                [pd.DataFrame([[
+                    statistic_file,
+                    100 * (country_positives / total),
+                    100 * (country_negatives / total),
+                    100 * (country_indeterminates / total),
+                    country_positives,
+                    country_negatives,
+                    country_indeterminates,
+                    100 * (city_positives / total),
+                    100 * (city_negatives / total),
+                    100 * (city_indeterminates / total),
+                    city_positives,
+                    city_negatives,
+                    city_indeterminates,
+                ]], columns=aggregation_df.columns,
+                ), aggregation_df],
+                ignore_index=True
+            )
+
+        aggregation_df.to_csv(HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH +
+                              "aggregation_results.csv", sep=",")
+
+
+statistics = Statistics("validation_unicast_udp_ripe_20230531_1",
+                        "validation_unicast_udp_ripe_20230531_1_no_check_multi_ip_target")
+#statistics.hunter_build_statistics_validation_campaign()
+statistics.aggregate_hunter_statistics()
 
 
