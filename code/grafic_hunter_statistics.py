4b3fb7f1c2038a1ebba3ca2d07704a8dfaa5783a
hugopascual <hugopascual998@gmail.com>
2023-06-22 01:02:19 +0200

    grafic bars hunter

--- code/grafic_hunter_statistics.py
+++ code/grafic_hunter_statistics.py
@@ -14,7 +14,7 @@ from utils.common_functions import (
 )
 
 
-def plot_grafics_aggregate(aggregation_df: pd.DataFrame, title: str):
+def plot_grafics_linear_aggregate(aggregation_df: pd.DataFrame, title: str):
     aggregation_df.sort_values(
         by=["moment_of_campaign", "validation"],
         inplace=True
@@ -48,7 +48,7 @@ def plot_grafics_aggregate(aggregation_df: pd.DataFrame, title: str):
             x=moment_of_campaign_list,
             y=aggregation_df[
                 aggregation_df["validation"] == validation
-                ]["country_negatives"],
+            ]["country_negatives"],
             line_color=line_colors_dict[validation],
             name=validation,
             legendgroup=validation,
@@ -70,12 +70,95 @@ def plot_grafics_aggregate(aggregation_df: pd.DataFrame, title: str):
     figure.show()
 
 
-def plot_grafic_by_country(country_code: str):
+def plot_grafics_bars_aggregate(aggregation_df: pd.DataFrame, title: str):
+    aggregation_df.sort_values(
+        by=["moment_of_campaign", "validation"],
+        inplace=True
+    )
+
+    moment_of_campaign_list = aggregation_df["moment_of_campaign"].unique()
+    validation_list = aggregation_df["validation"].unique()
+
+    country_results_list = ["country_positives",
+                            "country_indeterminates",
+                            "country_negatives"]
+    bars_colors_dict = dict(zip(
+        country_results_list,
+        ["green", "blue", "red"]
+    ))
+
+    figure = make_subplots(
+        rows=2,
+        cols=2,
+        subplot_titles=validation_list)
+
+    for country_result in country_results_list:
+        figure.add_trace(go.Bar(
+            x=moment_of_campaign_list,
+            y=aggregation_df[
+                aggregation_df["validation"] == validation_list[0]
+                ][country_result],
+            name=country_result,
+            legendgroup=country_result,
+            marker_color=bars_colors_dict[country_result]
+        ), row=1, col=1)
+
+    for country_result in country_results_list:
+        figure.add_trace(go.Bar(
+            x=moment_of_campaign_list,
+            y=aggregation_df[
+                aggregation_df["validation"] == validation_list[1]
+                ][country_result],
+            name=country_result,
+            legendgroup=country_result,
+            showlegend=False,
+            marker_color=bars_colors_dict[country_result]
+        ), row=1, col=2)
+
+    for country_result in country_results_list:
+        figure.add_trace(go.Bar(
+            x=moment_of_campaign_list,
+            y=aggregation_df[
+                 aggregation_df["validation"] == validation_list[2]
+                ][country_result],
+            name=country_result,
+            legendgroup=country_result,
+            showlegend=False,
+            marker_color=bars_colors_dict[country_result]
+        ), row=2, col=1)
+
+    for country_result in country_results_list:
+        figure.add_trace(go.Bar(
+            x=moment_of_campaign_list,
+            y=aggregation_df[
+                 aggregation_df["validation"] == validation_list[3]
+                 ][country_result],
+            name=country_result,
+            legendgroup=country_result,
+            showlegend=False,
+            marker_color=bars_colors_dict[country_result]
+        ), row=2, col=2)
+
+    figure.update_layout(title_text=title, barmode="stack")
+    figure.show()
+
+
+def plot_grafics_linear_by_country(country_code: str):
     aggregation_of_country_df = aggregate_one_country_hunter_statistics(
         country_code=country_code
     )
-    plot_grafics_aggregate(aggregation_of_country_df,
-                           title="Hunter results for {}".format(country_code))
+    plot_grafics_linear_aggregate(aggregation_of_country_df,
+                                  title="Hunter results for {}".format(
+                                      country_code))
+
+
+def plot_grafics_bars_by_country(country_code: str):
+    aggregation_of_country_df = aggregate_one_country_hunter_statistics(
+        country_code=country_code
+    )
+    plot_grafics_bars_aggregate(aggregation_of_country_df,
+                                title="Hunter results for {}".format(
+                                    country_code))
 
 
 def aggregate_one_country_hunter_statistics(country_code: str) -> pd.DataFrame:
@@ -155,6 +238,6 @@ total_aggregation_df = pd.read_csv(
     sep=","
 )
 
-plot_grafics_aggregate(total_aggregation_df,
-                       title="Hunter results aggregated")
-plot_grafic_by_country(country_code="IT")
+plot_grafics_bars_aggregate(total_aggregation_df,
+                            title="Hunter results aggregated")
+plot_grafics_bars_by_country(country_code="IT")
