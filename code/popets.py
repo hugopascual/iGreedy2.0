c5d5d09f1058ca73df05abddb942814c02a223c8
hugopascual <hugopascual998@gmail.com>
2023-06-27 10:52:16 +0200

    popets statistics

--- code/popets.py
+++ code/popets.py
@@ -6,15 +6,19 @@ import pandas as pd
 import subprocess
 import ipinfo
 import socket
+from shapely import from_geojson
 
 from utils.constants import (
     KEY_FILEPATH,
-    HUNTER_MEASUREMENTS_CAMPAIGNS_PATH
+    HUNTER_MEASUREMENTS_CAMPAIGNS_PATH,
+    HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH
 )
 from utils.common_functions import (
     dict_to_json_file,
     json_file_to_dict,
-    countries_in_EEE_set
+    countries_in_EEE_set,
+    get_list_files_in_path,
+    get_nearest_airport_to_point
 )
 from hunter import Hunter
 
@@ -174,4 +178,149 @@ def connect_to_vpn_server(vpn_server: str) -> dict:
         }
 
 
-hunt_popets_anycast()
+def generata_popets_results(validate_last_hop: bool,
+                            validate_target: bool,
+                            campaign_name: str):
+    campaign_filepath = HUNTER_MEASUREMENTS_CAMPAIGNS_PATH + campaign_name
+    countries_in_EEE = countries_in_EEE_set()
+    results_filenames_list = get_list_files_in_path(campaign_filepath)
+
+    popets_jumps_df = pd.DataFrame(columns=[
+        "target", "origin_name", "origin_code", "destination_code",
+        "out_of_EEE", "reason"
+    ])
+    for result_filename in results_filenames_list:
+        print(result_filename)
+        result_filepath = campaign_filepath + "/" + result_filename
+        result_dict = json_file_to_dict(result_filepath)
+
+        origin_code = result_dict["additional_info"]["server_name"].split(
+            "#")[0]
+
+        (destination_code, reason) = calculate_hunter_result_outcome(
+            results=result_dict,
+            validate_last_hop=validate_last_hop,
+            validate_target=validate_target
+        )
+
+        if (destination_code in countries_in_EEE) or \
+                destination_code == "Indeterminate":
+            out_of_EEE = False
+        else:
+            out_of_EEE = True
+
+        popets_jumps_df = pd.concat([pd.DataFrame([[
+            result_dict["target"],
+            result_dict["additional_info"]["country"],
+            origin_code,
+            destination_code,
+            bool(out_of_EEE),
+            reason
+        ]], columns=popets_jumps_df.columns),
+            popets_jumps_df
+        ], ignore_index=True)
+
+    popets_jumps_df.sort_values(by=["out_of_EEE",
+                                    "target",
+                                    "destination_code",
+                                    "origin_code"],
+                                ascending=[False, True, True, True],
+                                inplace=True)
+    if validate_target and validate_last_hop:
+        validation_name = "ip_all_validation"
+    elif validate_target:
+        validation_name = "ip_target_validation"
+    elif validate_last_hop:
+        validation_name = "ip_last_hop_validation"
+    else:
+        validation_name = "no_ip_validation"
+    popets_jumps_df.to_csv(HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH +
+                           "{}_{}.csv".format(campaign_name, validation_name),
+                           sep=",",
+                           index=False)
+
+
+def calculate_hunter_result_outcome(
+        results: dict,
+        validate_last_hop: bool = True,
+        validate_target: bool = True) -> (str, str):
+    result_param = "countries"
+    gt_param = "country_code"
+
+    if validate_last_hop:
+        try:
+            if len(results["hops_directions_list"][-2]) != 1:
+                return "Indeterminate", "Multiples IP on last_hop"
+            elif results["hops_directions_list"][-1] == "*":
+                return "Indeterminate", "Last_hop hop do not respond"
+        except:
+            print("Target directions list exception")
+            return "Indeterminate", "Exception in last_hop validation"
+
+    if validate_target:
+        try:
+            if len(results["hops_directions_list"][-1]) != 1:
+                return "Indeterminate", "Multiples IP on target hop"
+            elif results["hops_directions_list"][-1] == "*":
+                return "Indeterminate", "Target hop do not respond"
+        except:
+            print("Target directions list exception")
+            return "Indeterminate", "Exception in target validation"
+
+    # Num of results validation
+    num_result_countries = len(results["hunt_results"]["countries"])
+    if num_result_countries == 1:
+        return results["hunt_results"][result_param][0], "Result intersection"
+    elif num_result_countries == 0:
+        centroid = from_geojson(results["hunt_results"]["centroid"])
+        if not results["last_hop"]["ip"]:
+            return "Indeterminate", "Last hop not valid"
+        elif not results["last_hop"]["geolocation"]:
+            return "Indeterminate", "Last hop not geolocated"
+        elif not results["discs_intersect"]:
+            return "Indeterminate", "Ping discs no intersection"
+        elif not centroid:
+            return "Indeterminate", "Centroid not found"
+        else:
+            nearest_airport = get_nearest_airport_to_point(centroid)
+            return nearest_airport[gt_param], "Result nearest airport"
+    else:
+        return "Indeterminate", "Too many locations"
+
+
+def get_ip_out_EEE_list(filename: str) -> list:
+    jumps_popets_df = pd.read_csv(
+        HUNTER_MEASUREMENTS_CAMPAIGNS_STATISTICS_PATH + filename,
+        sep=","
+    )
+
+    jumps_popets_df = jumps_popets_df[
+        jumps_popets_df["out_of_EEE"] == bool(True)]
+    return list(jumps_popets_df["target"].unique())
+
+
+#hunt_popets_anycast()
+#generata_popets_results(
+#    validate_last_hop=True,
+#    validate_target=True,
+#    campaign_name="PoPETs_anycast_ipinfo"
+#)
+#generata_popets_results(
+#    validate_last_hop=False,
+#    validate_target=True,
+#    campaign_name="PoPETs_anycast_ipinfo"
+#)
+#generata_popets_results(
+#    validate_last_hop=True,
+#    validate_target=False,
+#    campaign_name="PoPETs_anycast_ipinfo"
+#)
+#generata_popets_results(
+#    validate_last_hop=False,
+#    validate_target=False,
+#    campaign_name="PoPETs_anycast_ipinfo"
+#)
+print(get_ip_out_EEE_list("PoPETs_anycast_ipinfo_ip_target_validation.csv"))
+print(
+    len(get_ip_out_EEE_list("PoPETs_anycast_ipinfo_ip_target_validation.csv"))
+)
